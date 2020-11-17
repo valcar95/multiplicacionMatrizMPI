0521627990f464a5cfcd95dd20fcccaa0239ccb4
Victor Cardona <va.cv95@gmail.com>
2020-11-17 03:57:07 -0500

    mpi

--- mat_vec_mult_mpi2.c
+++ mat_vec_mult_mpi2.c
@@ -120,9 +120,9 @@ void mat_vect_mult(double* A, double* local_x, double* local_y, int n, int it, i
         for (j = 0 ; j<n ; j++ )
         {
             local_y[i] = 0.0 ;
-            for ( int k = 0 ; k<n ; k++ )
+            for ( int k = 0 ; k<local_n ; k++ )
             {
-                local_y[i] += local_x[i]*A[k*n+j];
+                local_y[i] += local_x[k]*A[k*n+j];
             }
         }
     }
