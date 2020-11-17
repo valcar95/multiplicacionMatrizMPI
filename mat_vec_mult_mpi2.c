d4e478785c5907758f745d127f5298ab76fd1c62
Victor Cardona <va.cv95@gmail.com>
2020-11-17 03:41:48 -0500

    mpi

--- mat_vec_mult_mpi2.c
+++ mat_vec_mult_mpi2.c
@@ -111,6 +111,7 @@ void gen_data(double * array, int size){
 }
 
 void mat_vect_mult(double* A, double* local_x, double* local_y, int n, int it, int p){
+  printf("----entra----\n")
   int h, i, j,k,local_n;
   local_n=n/p;
   for(h = 0; h < it; h++){
@@ -140,6 +141,7 @@ void mat_vect_mult(double* A, double* local_x, double* local_y, int n, int it, i
         local_x[i] = local_y[i];
     }
   }
+  printf("----sale----\n")
 }
 
 void print_vector(char* name, double*  y, int m) {
