0644e393e6121b28455442146093024681016f29
Victor Cardona <va.cv95@gmail.com>
2020-11-16 23:56:25 -0500

    mpi

--- mat_vec_mult_mpi.c
+++ mat_vec_mult_mpi.c
@@ -67,13 +67,13 @@ int main()
   local_x=malloc(sizeof(double)*n_per_proc);
   local_y=malloc(sizeof(double)*n_per_proc);
 
-  double AA[16]={1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0};
-  double CC[1];
+  int AA[16]={1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8};
+  int CC[1];
 
   
 
-  MPI_Scatter(AA, 16/comm_sz, MPI_DOUBLE, CC, 16/comm_sz, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  printf("sale de scater  from process=%d CC[0]=%lf\n",my_rank,CC[0]);
+  MPI_Scatter(AA, 16/comm_sz, MPI_INT, CC, 16/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
+  printf("sale de scater  from process=%d CC[0]=%d\n",my_rank,CC[0]);
   MPI_Barrier(MPI_COMM_WORLD);
   if(my_rank==0){
     printf("-----------sale de la barrera-------------\n"); 
